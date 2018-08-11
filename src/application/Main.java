d3bd226b4ce82289f4e6261fbaa30d8b19969e48
rilakkyuma <timmv14489@gmail.com>
2018-08-11 02:18:57 -0400

    added defn. serialization export/import

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import webscraping.KpediaWebscraper;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -12,6 +15,8 @@ public class Main extends Application {
 	
 	@Override
     public void start(Stage primaryStage) throws Exception {
+		
+		KpediaWebscraper.getInstance().readInSerialization();
 
         // just load fxml file and display it in the stage:
 
@@ -19,6 +24,14 @@ public class Main extends Application {
         loader.setController(new FXMLController());
         Parent root = loader.load();
         Scene scene = new Scene(root);
+        
+        //when the program is closed, export the definitions serialization to a .ser file
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent t) {
+            	KpediaWebscraper.getInstance().exportSerialization();
+            }
+        });
+        
         primaryStage.getIcons().add(new Image("assets/icon.png"));
         primaryStage.setTitle("Korean Lookup");
         primaryStage.setScene(scene);
