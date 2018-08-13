6c0a229f5f901bb4330289f611364ae6f7696102
rilakkyuma <timmv14489@gmail.com>
2018-08-13 04:46:26 -0400

    contained export serialization methods to this class

--- src/sweep/Sweeper.java
+++ src/sweep/Sweeper.java
@@ -1,11 +1,21 @@
 package sweep;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import hangeul.Data;
 import webscraping.KpediaWebscraper;
 import webscraping.Mode;
 import webscraping.WiktionaryWebscraper;
 
 public class Sweeper {
+	
+	public static final String conjugationsSweepTo = "src/sweep/conjugations.ser";
+	public static final String definitionsSweepTo = "src/sweep/definitions.ser";
 	
 	/**
 	 * Method that sweeps Kpedia and Wiktionary, filling in the respective memoized HashMaps
@@ -43,9 +53,65 @@ public class Sweeper {
 			
 		}
 		
-		KpediaWebscraper.getInstance().exportSerialization();
-		WiktionaryWebscraper.getInstance().exportSerialization();
+		exportDefinitionsSerialization();
+		exportConjugationsSerialization();
 		
 	}
+	
+	public static void throwException() throws DoNotExecuteThisCodeException {
+		throw new DoNotExecuteThisCodeException();
+	}
+	
+	
+	//export the serialization of the definition memo HashMap
+	public static void exportDefinitionsSerialization() {
+		try {
+	         FileOutputStream filestream = new FileOutputStream(Sweeper.definitionsSweepTo);
+	         ObjectOutputStream objectstream = new ObjectOutputStream(filestream);
+	         objectstream.writeObject(KpediaWebscraper.getInstance().getDefinitionsMemo());
+	         objectstream.close();
+	         filestream.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	//export the serialization of the conjugation memo HashMap
+	public static void exportConjugationsSerialization() {
+		try {
+		     FileOutputStream filestream = new FileOutputStream(Sweeper.conjugationsSweepTo);
+		     ObjectOutputStream objectstream = new ObjectOutputStream(filestream);
+		     objectstream.writeObject(WiktionaryWebscraper.getInstance().getConjugationsMemo());
+		     objectstream.close();
+		     filestream.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws DoNotExecuteThisCodeException {
+		
+		throwException();
+		
+		try {
+			File file = new File("src/sweep/words");
+
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+				stringBuffer.append(" ");
+			}
+			String[] splitted = stringBuffer.toString().split(" ");
+			Sweeper.sweep(splitted);
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 
 }
